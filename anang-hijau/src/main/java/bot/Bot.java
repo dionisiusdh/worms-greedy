71da279be37ac6c9d53d49e0dc7b2a1dea18941e
Josep Marcello <jspmarcello@live.com>
2021-02-10 21:41:20 +0700

    feat(Bot, move): new move algorithm

--- anang-hijau/src/main/java/bot/Bot.java
+++ anang-hijau/src/main/java/bot/Bot.java
@@ -5,23 +5,46 @@ import bot.entities.*;
 import bot.enums.CellType;
 import bot.enums.Direction;
 
+// import java.lang.reflect.Array;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class Bot {
+    /** ANANG GREEN **
+     * List fungsi:
+     * - ctor Bot
+     * - getCurrentWorm()
+     * - getFirstWormInRange()
+     * - constructFireDirectionLines() 
+     * - getSurroundingCells()
+     * - euclideanDistance()
+     * - isValidCoordinate()
+     * - resolveDirection()
+     * - run()
+     */
 
     private Random random;
     private GameState gameState;
     private Opponent opponent;
     private MyWorm currentWorm;
 
+    /**
+     * Metode constructor bot
+     * @param random ???
+     * @param gameState ???
+     */
     public Bot(Random random, GameState gameState) {
         this.random = random;
         this.gameState = gameState;
         this.opponent = gameState.opponents[0];
         this.currentWorm = getCurrentWorm(gameState);
     }
-
+    
+    /**
+     * Metode untuk mendapatkan worm bot bot
+     * @param gameState gameState pada suatu ronde
+     * @return array of worms milik bot
+     */
     private MyWorm getCurrentWorm(GameState gameState) {
         return Arrays.stream(gameState.myPlayer.worms)
                 .filter(myWorm -> myWorm.id == gameState.currentWormId)
@@ -29,29 +52,11 @@ public class Bot {
                 .get();
     }
 
-    public Command run() {
-
-        Worm enemyWorm = getFirstWormInRange();
-        if (enemyWorm != null) {
-            Direction direction = resolveDirection(currentWorm.position, enemyWorm.position);
-            return new ShootCommand(direction);
-        }
-
-        List<Cell> surroundingBlocks = getSurroundingCells(currentWorm.position.x, currentWorm.position.y);
-        int cellIdx = random.nextInt(surroundingBlocks.size());
-
-        Cell block = surroundingBlocks.get(cellIdx);
-        if (block.type == CellType.AIR) {
-            return new MoveCommand(block.x, block.y);
-        } else if (block.type == CellType.DIRT) {
-            return new DigCommand(block.x, block.y);
-        }
-
-        return new DoNothingCommand();
-    }
-
+    /**
+     * Metode untuk mendapatkan worm pertama yang dapat ditembak
+     * @return worm musuh pertama yang dapat ditembak
+     */
     private Worm getFirstWormInRange() {
-
         Set<String> cells = constructFireDirectionLines(currentWorm.weapon.range)
                 .stream()
                 .flatMap(Collection::stream)
@@ -68,6 +73,11 @@ public class Bot {
         return null;
     }
 
+    /**
+     * ???
+     * @param range ???
+     * @return ???
+     */
     private List<List<Cell>> constructFireDirectionLines(int range) {
         List<List<Cell>> directionLines = new ArrayList<>();
         for (Direction direction : Direction.values()) {
@@ -98,6 +108,12 @@ public class Bot {
         return directionLines;
     }
 
+    /**
+     * Metode untuk menentukan sel di sekitar suatu titik
+     * @param x absis titik
+     * @param y ordinat titik
+     * @return List dari sel
+     */
     private List<Cell> getSurroundingCells(int x, int y) {
         ArrayList<Cell> cells = new ArrayList<>();
         for (int i = x - 1; i <= x + 1; i++) {
@@ -112,10 +128,51 @@ public class Bot {
         return cells;
     }
 
+    /**
+     * Metode untuk mendapatkan sel di sekitar musuh yang terdekat dengan salah
+     * satu worm milik bot
+     * @param currentWorm array list berisi worm milik bot
+     * @param enemyWorm array list berisi worm milik musuh
+     * @return sel terdekat antara salah satu worm milik musuh dengan salah satu
+     * worm milik bot
+     */
+    private Cell getCellNearestToEnemyWorm(Worm currentWorm, Worm enemyWorm){
+        List<Cell> surroundingCells = getSurroundingCells(currentWorm.position.x,
+                                                    currentWorm.position.y);
+        int minDistance = 9999999;
+        int currDistance;
+        Cell nearestCell = surroundingCells.get(0);
+        
+        for (Cell cell : surroundingCells){
+            currDistance = euclideanDistance(currentWorm.position.x,
+                                        currentWorm.position.y, cell.x, cell.y);
+            if (currDistance <= minDistance){
+                minDistance = currDistance;
+                nearestCell = cell;
+            }
+        }
+
+        return nearestCell;
+    }
+
+    /**
+     * Metode untuk menghitung jarak euclidean dari dua titik, a dan b
+     * @param aX absis titik a
+     * @param aY ordinat titik a
+     * @param bX absis titik b
+     * @param bY ordinat titik b
+     * @return Jarak titik a dan b
+     */
     private int euclideanDistance(int aX, int aY, int bX, int bY) {
         return (int) (Math.sqrt(Math.pow(aX - bX, 2) + Math.pow(aY - bY, 2)));
     }
 
+    /**
+     * Metode untuk menentukan apakah koordinat valid
+     * @param x absis dari titik
+     * @param y ordinat dari titik
+     * @return true jika koordinat valik, false jika tidak
+     */
     private boolean isValidCoordinate(int x, int y) {
         return x >= 0 && x < gameState.mapSize
                 && y >= 0 && y < gameState.mapSize;
@@ -140,5 +197,148 @@ public class Bot {
         }
 
         return Direction.valueOf(builder.toString());
+    }
+
+    private Position getGreedyMove(Worm currentWorm) {
+        ArrayList<Worm> enemyWorms = getEnemyWorms();
+        ArrayList<Worm> friendlyWorms = getFriendlyWorms();
+        Worm targetWorm;
+        Position targetPosition;
+        Cell targetCell;
+        
+        // TODO: Mungkin bisa dibikin lebih mangkus
+        if (isExistGoodWorm(enemyWorms)){
+            targetWorm = getLowestHPWorm(enemyWorms);
+        } else {
+            targetWorm = getNearestWorm(friendlyWorms, enemyWorms);
+        }
+
+        targetCell = getCellNearestToEnemyWorm(currentWorm, targetWorm);
+        targetPosition = new Position(targetCell.x, targetCell.y);
+        
+        return targetPosition;
+    }
+
+    /**
+     * Metode untuk membuat ArrayList berisi worms milik bot
+     * @return ArrayList berisi worms milik bot
+     */
+    private ArrayList<Worm> getFriendlyWorms() {
+        ArrayList<Worm> worms = new ArrayList<Worm>();
+        
+        for (Worm friendlyWorm : gameState.myPlayer.worms) {
+            worms.add(friendlyWorm);
+        }
+
+        return worms;
+    }
+
+    /**
+     * Metode untuk membuat ArrayList berisi worms musuh
+     * @return ArrayList berisi worms musuh
+     */
+    private ArrayList<Worm> getEnemyWorms() {
+        ArrayList<Worm> worms = new ArrayList<Worm>();
+        
+        for (Worm enemyWorm : gameState.opponents[0].worms) {
+            worms.add(enemyWorm);
+        }
+
+        return worms;
+    }
+
+    /**
+     * Metode untuk mendapatkan bot dengan HP terendah
+     * @param enemyWorms ArrayList of worms milik musuh
+     * @return list worm dengan HP terendah
+     */
+    private Worm getLowestHPWorm(ArrayList<Worm> enemyWorms) {
+        int leastWormHp = enemyWorms.get(0).health;
+        Worm selected = enemyWorms.get(0);
+
+        for (Worm worm : enemyWorms){
+            if (worm.health < leastWormHp) {
+                selected = worm;
+            }
+        }
+
+        return selected;
+    }
+
+    /**
+     * Metode untuk mendapatkan bot terdekat
+     * @param friendlyWorms ArrayList of worms milik bot
+     * @param enemyWorms ArrayList of worms milik musuh
+     * @return worm dengan jarak terdekat
+     */
+    private Worm getNearestWorm(ArrayList<Worm> friendlyWorms, ArrayList<Worm> enemyWorms) {
+        int enemyX, enemyY;
+        Worm nearestEnemyWorm = enemyWorms.get(0);
+        int minDistance = 9999999;
+        int currDistance;
+
+        for (Worm friendlyWorm : friendlyWorms){
+            for (Worm enemyWorm : enemyWorms){
+                enemyX = enemyWorm.position.x;
+                enemyY = enemyWorm.position.y;
+                currDistance = euclideanDistance(friendlyWorm.position.x, friendlyWorm.position.y, enemyX, enemyY);
+
+                if (currDistance < minDistance){
+                    minDistance = currDistance;
+                    nearestEnemyWorm = enemyWorm;
+                }
+            }
+        }
+        
+        return nearestEnemyWorm;
+    }
+
+    /**
+     * Metode untuk menentukan keberadaan worm dengan selisih HP lebih besar
+     * dari batas
+     * @param enemyWorms ArrayList berisi worm milik musuh
+     * @return true jika ada worm yang memiliki HP dengan selisih lebih besar
+     * dari batas
+     */
+    private boolean isExistGoodWorm(ArrayList<Worm> enemyWorms) {
+        int lowestHp = 99999999;
+        for (Worm worm: enemyWorms) {
+            if (lowestHp > worm.health && worm.health > 0) {
+                lowestHp = worm.health;
+            }
+        }
+
+        for (Worm worm: enemyWorms) {
+            if (worm.health - lowestHp >= 20) return true;
+        }
+
+        return false;
+    }
+
+
+
+    /**
+     * Metode untuk menjalankan bot
+     * @return command yang ingin dijalankan
+     */
+    public Command run() {
+        Worm enemyWorm = getFirstWormInRange();
+        Position targePosition = getGreedyMove(currentWorm);
+        if (enemyWorm != null) {
+            Direction direction = resolveDirection(currentWorm.position, enemyWorm.position);
+            return new ShootCommand(direction);
+        }
+
+        List<Cell> surroundingBlocks = getSurroundingCells(currentWorm.position.x, currentWorm.position.y);
+        int cellIdx = random.nextInt(surroundingBlocks.size());
+
+        Cell block = surroundingBlocks.get(cellIdx);
+        if (block.type == CellType.AIR) {
+            return new MoveCommand(block.x, block.y);
+        } else if (block.type == CellType.DIRT) {
+            return new DigCommand(block.x, block.y);
+        }
+
+        return new DoNothingCommand();
     }
 }
--- anang-hijau/src/main/java/bot/entities/Position.java
+++ anang-hijau/src/main/java/bot/entities/Position.java
@@ -3,9 +3,21 @@ package bot.entities;
 import com.google.gson.annotations.SerializedName;
 
 public class Position {
+    /// nilai absis position
     @SerializedName("x")
     public int x;
 
+    /// nilai absis position
     @SerializedName("y")
     public int y;
+
+    /**
+     * Constructor untuk class Position
+     * @param x nilai absis
+     * @param y nilai ordinat
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 }
